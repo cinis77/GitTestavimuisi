f7f5d42f7d1b000ed8fe2299939eb3af074426cc
Ernestas Žemaitis <zemaitis.ernestas@gmail.com>
2020-06-29 20:16:36 +0300

    pakeitimai

--- GitTestavimuisi-master/GitTestavimuisi/Program.cs
+++ GitTestavimuisi-master/GitTestavimuisi/Program.cs
@@ -15,23 +15,26 @@ namespace GitTestavimuisi
 
     class Komunikacija : IKomunikacija
     {
+        public string Message { get; set; }
         public void PakartotiPaskutineZinute()
         {
-            
+			Console.WriteLine(Message);
         }
 
         public void Siusti(string zinute)
         {
-         
+			Console.WriteLine(zinute); 
         }
 
         public string SkaitytiDuomenis()
         {
-
-            return "duomenys";
-
+            Console.WriteLine("Entrer Message:");
+            Message = Console.ReadLine();
+            return Message;
         }
     }
+
+ 
 
     class Duomenu_paruosimas : IDuomenu_paruosimas
     {
@@ -39,22 +42,22 @@ namespace GitTestavimuisi
 
         public void PridetiDuomeni(IDuomenys duomuo)
         {
-            
+            Data.Add(duomuo);
         }
 
         public void SiustiDuomeni(int kelintas)
         {
-            
+            Console.WriteLine(Data[kelintas]);
         }
 
         public void SiustiPaskutinDuomeni()
         {
-            
+            Console.WriteLine(Data[Data.Count() - 1]);
         }
 
         public void SiustiPirmaDuomeni()
         {
-            
+            Console.WriteLine(Data[0]);
         }
 
         public string SukurtiDuomenuEilute(IDuomenys duomuo)
