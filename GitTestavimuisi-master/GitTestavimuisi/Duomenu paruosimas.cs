f0ebb8486ea5d5ef21397da8580294aef9a9a1a0
Ramunė Alionytė <66987985+celly666@users.noreply.github.com>
2020-06-29 19:57:02 +0300

    Update Duomenu paruosimas.cs

--- GitTestavimuisi-master/GitTestavimuisi/Duomenu paruosimas.cs
+++ GitTestavimuisi-master/GitTestavimuisi/Duomenu paruosimas.cs
@@ -13,12 +13,31 @@ namespace GitTestavimuisi
         DateTime DuomenuParuososLaikas { get; set; }
 
     }
+    class Duomenys: IDuomenys
+    {
+    int Id {get; set;}
+    string Zinute { ger;set;}
+    DateTime DuomenuParuosimasLaikas {get;set;}
+    
 
     interface IKomunikacija
     {
         void Siusti(string zinute);
         string SkaitytiDuomenis();
         void PakartotiPaskutineZinute();
+    }
+    class Komunikacija : IKomunikacija
+    {
+    public void PakartotiPaskutineZinute()
+    {
+    }
+    public void Siusti (string zinute)
+    {
+    }
+    public string SkaitytiDuomenis()
+    {
+    return "a";
+    }
     }
 
     interface IDuomenu_paruosimas
@@ -34,5 +53,28 @@ namespace GitTestavimuisi
         void SiustiDuomeni(int kelintas);
 
         void SiustiPaskutinDuomeni();
+    }
+    class Duomenu_paruosimas:IDuomenu_paruosimas
+    {
+    public List<IDuomenys> Data{get;}
+    
+    public void PridetiDuomeni(IDuomenys duomuo)
+    {
+    
+    }
+    public void SiustiDuomeni(int kelintas)
+    {
+    }
+    public void SiustiPaskutiniDuomeni()
+    {
+    }
+    public void SiustiPirmaDuomeni()
+    {
+    }
+    public string SukurtiDuomenuEilute(IDuomenys duomuo)
+    {
+    string a="shsjfjsjfs";
+    return a;
+    }
     }
 }
