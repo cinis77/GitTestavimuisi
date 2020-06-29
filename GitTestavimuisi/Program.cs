76ebbcd20a03e0501b391da625a8fb28d7099793
cinis77 <cinis.dwarnius.minis@gmail.com>
2020-06-29 14:58:45 +0300

    Update for class

--- GitTestavimuisi/Program.cs
+++ GitTestavimuisi/Program.cs
@@ -6,63 +6,7 @@ using System.Threading.Tasks;
 
 namespace GitTestavimuisi
 {
-    class Duomenys : IDuomenys
-    {
-        public int Id { get; set; }
-        public string Zinute { get; set; }
-        public DateTime DuomenuParuososLaikas { get; set; }
-    }
-
-    class Komunikacija : IKomunikacija
-    {
-        public void PakartotiPaskutineZinute()
-        {
-
-        }
-
-        public void Siusti(string zinute)
-        {
-
-        }
-
-        public string SkaitytiDuomenis()
-        {
-            string dom = null;
-            return dom;
-        }
-    }
-
-
-    class Duomenu_paruosimas : IDuomenu_paruosimas
-    {
-        public List<IDuomenys> Data { get; }
-
-        public void PridetiDuomeni(IDuomenys duomuo)
-        {
-
-        }
-
-        public void SiustiDuomeni(int kelintas)
-        {
-
-        }
-
-        public void SiustiPaskutinDuomeni()
-        {
-
-        }
-
-        public void SiustiPirmaDuomeni()
-        {
-
-        }
-
-        public string SukurtiDuomenuEilute(IDuomenys duomuo)
-        {
-            string domuo = null;
-            return domuo;
-        }
-}
+   
 
 
 class Program
